2504b997083dc6aa0b43119b04450929e7fbd909
Unknown <kayden18@ymail.com>
2017-01-27 10:54:55 -0700

    rotation and classes

--- New Unity Project/Assets/changeColor.cs
+++ New Unity Project/Assets/changeColor.cs
@@ -4,35 +4,29 @@ public class changeColor : MonoBehaviour
 {
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
+        if (Input.GetKeyDown(KeyCode.R)){
             GetComponent<Renderer>().material.color = Color.red;
             print("Changed color to red");
         }
 
-        if (Input.GetKeyDown(KeyCode.G))
-        {
+        if (Input.GetKeyDown(KeyCode.G)){
             GetComponent<Renderer>().material.color = Color.green;
             print("Changed color to green");
         }
 
-        if (Input.GetKeyDown(KeyCode.B))
-        {
+        if (Input.GetKeyDown(KeyCode.B)){
             GetComponent<Renderer>().material.color = Color.blue;
             print("Changed color to blue");
         }
-        if (Input.GetKeyDown(KeyCode.Delete))
-        {
+        if (Input.GetKeyDown(KeyCode.Delete)){
             GetComponent<Renderer>().material.color = Color.white;
             print("Changed color to white");
         }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space)){
             GetComponent<Renderer>().material.color = Color.black;
             print("Changed color to black");
         }
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
+        if (Input.GetKeyDown(KeyCode.Return)){
             GetComponent<Renderer>().material.color = Color.yellow;
             print("Changed color to yellow");
         }
